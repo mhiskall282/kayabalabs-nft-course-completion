c6f0f37dcaab51522e3e8afa985d526328f2e861
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-01-15 00:33:26 +0000

    Update KayabaAchievementNFT.t.sol
    first test case

--- test/KayabaAchievementNFT.t.sol
+++ test/KayabaAchievementNFT.t.sol
@@ -3,3 +3,12 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/KayabaAchievementNFT.sol";
+
+contract KayabaAchievementNFTTest is Test {
+    KayabaAchievementNFT public nft;
+    address public owner;
+    address public student1;
+    address public student2;
+    
+    uint256 constant MINT_FEE = 0.0003 ether;
+    
