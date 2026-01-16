b37f44c475132acbeb2acde95ce8c549837885e3
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-01-16 12:10:35 +0000

    Update KayabaCourseCompletionNFT.sol

--- src/KayabaCourseCompletionNFT.sol
+++ src/KayabaCourseCompletionNFT.sol
@@ -256,4 +256,12 @@ contract KayabaCourseCompletionNFT is ERC721, ERC721URIStorage, Ownable {
         return super.tokenURI(tokenId);
     }
 
-    
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
